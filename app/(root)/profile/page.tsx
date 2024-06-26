9bffcac34f14154cbd41ef5595e5075fd1fa744e
Christine Wong <jumiryoo@gmail.com>
2024-06-26 13:43:52 -0700

    added pagination for events

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,14 +1,24 @@
 import Collection from '@/components/shared/Collection'
 import { Button } from '@/components/ui/button'
 import { getEventsByUser } from '@/lib/actions/event.actions'
+import { getOrdersByUser } from '@/lib/actions/order.actions'
+import { IOrder } from '@/lib/database/models/order.model'
+import { SearchParamProps } from '@/types'
 import { auth } from '@clerk/nextjs'
 import Link from 'next/link'
 import React from 'react'
 
-const ProfilePage = async () => {
+const ProfilePage = async ({ searchParams }: SearchParamProps) => {
 
     const { sessionClaims } = auth();
     const userId = sessionClaims?.userId as string;
+
+    const ordersPage = Number(searchParams?.ordersPage) || 1;
+    const eventsPage = Number(searchParams?.eventssPage) || 1;
+
+
+    const orders = await getOrdersByUser({ userId, page: 1})
+    const orderedEvents = orders?.data.map((order: IOrder) => order.event) || [];
 
     const organizedEvents = await getEventsByUser({ userId, page: 1})
   return (
@@ -25,7 +35,7 @@ const ProfilePage = async () => {
     </section>
 
     <section className='wrapper my-8'> 
-    {/* <Collection 
+    <Collection 
           data={orderedEvents}
           emptyTitle="No event tickets purchased yet"
           emptyStateSubtext="No worries - plenty of exciting events to explore!"
@@ -34,7 +44,7 @@ const ProfilePage = async () => {
           page={ordersPage}
           urlParamName="ordersPage"
           totalPages={orders?.totalPages}
-        /> */}
+        />
     </section>
 
     <section className='bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10'>
@@ -55,7 +65,7 @@ const ProfilePage = async () => {
           emptyStateSubtext="Create your first event"
           collectionType="Events_Organized"
           limit={3}
-          page={1}
+          page={eventsPage}
           urlParamName="eventsPage"
           totalPages={organizedEvents?.totalPages}
         />
